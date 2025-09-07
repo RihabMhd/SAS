002665300602ab0fca1430cda2271b9488ba5c54
RihabMhd <rihabmahdi19@gmail.com>
2025-09-07 21:00:18 +0100

    Done

--- DayThree/chapitre3/Ex9.c
+++ DayThree/chapitre3/Ex9.c
@@ -7,9 +7,7 @@ l'alphabet. Par exemple, si chaque lettre est remplacée par la lettre deux posi
 après elle, alors Y serait remplacé par A, et Z serait remplacé par B.)
 Écrivez un programme qui chiffre un message en utilisant un chiffrement de César.
 L'utilisateur entrera le message à chiffrer et le décalage (le nombre de positions de
-décalage des lettres) :
-
-7
+décalage des lettres) :7
 Entrez le message à chiffrer : Go ahead, make my day.
 Entrez le décalage (1-25) : 3
 Message chiffré : Jr dkhdg, pdnh pb gdb.
@@ -22,4 +20,32 @@ Indice : Pour gérer le problème du "retour" (wrap-around), utilisez l'expressi
 - 'A') + n) % 26 + 'A' pour calculer la version chiffrée d'une lettre majuscule,
 où ch stocke la lettre et n stocke le décalage. (Vous aurez besoin d'une expression
 similaire pour les lettres minuscules.)
+Prends une majuscule, calcule sa position dans l’alphabet (0–25), ajoute le décalage, 
+boucle si nécessaire, puis retransforme en lettre majuscule
 */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(){
+    int decalage;
+    char message[100],messageChiffrer[100];
+
+    printf("Entrez le message a chiffrer :");
+    fgets(message, sizeof(message), stdin);
+
+    printf("Entrez le decalage (1-25) :");
+    scanf("%d",&decalage);
+    if(decalage<25 && decalage>1){
+        for(int i=0; i<sizeof(message)/sizeof(message[0]);i++){
+        if(message[i]>='A' && message[i]<='Z') messageChiffrer[i]=((message[i]-'A') + decalage)%26+'A';
+        else if(message[i]>='a' && message[i]<='z') messageChiffrer[i]=((message[i]-'a') + decalage)%26+'a';
+        else messageChiffrer[i]=message[i];
+        printf("%c",messageChiffrer[i]);
+    }
+    }else{
+        printf("invalide");
+    }
+
+    
+
+}
